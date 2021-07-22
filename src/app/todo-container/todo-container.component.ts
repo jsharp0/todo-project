84cbb687f2c29be1e79cfdd83b1f3f5e69eda656
Jenna Sharp <sharp.jkm@gmail.com>
2021-07-22 16:56:28 -0400

    add functionality to filters

--- src/app/todo-container/todo-container.component.ts
+++ src/app/todo-container/todo-container.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Todo } from '../store/Models/todo.model';
+import { State } from '../store/Models/state.model';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
-  styleUrls: ['./todo-container.component.scss']
+  styleUrls: ['./todo-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoContainerComponent implements OnInit {
   todoList: Todo[] = [
     {completed: false, text: 'Testing'}
   ];
 
-  constructor() { }
+  filteredTodoList: Todo[] = [];
+
+  currentFilter: string = null;
+
+  constructor(private store: Store<State>) { }
 
   ngOnInit(): void {
+    this.store.select(todos => todos).subscribe(todoList => {
+      this.todoList = todoList.todo;
+    });
+
+    this.filteredTodoList = this.todoList;
+  }
+
+  filterItems(filter?: string): void {
+    this.currentFilter = filter;
+    console.log(this.currentFilter);
+    switch (filter) {
+    case 'completed':
+        this.filteredTodoList = this.todoList.filter(todo => todo.completed);
+        // this.currentFilter = 'completed'
+        break;
+    case 'active':
+        this.filteredTodoList = this.todoList.filter(todo => !todo.completed);
+        // this.currentFilter = 'active';
+        break;
+      default:
+        this.filteredTodoList  = this.todoList;
+        // this.currentFilter = null;
+        break;
+    }
+    console.log(this.filteredTodoList);
   }
 
 }
--- src/app/ui-components/checkbox/checkbox.component.ts
+++ src/app/ui-components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -6,7 +6,7 @@ import { Component, ElementRef, Input, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
-  checked: boolean;
+  @Input() checked: boolean;
   @Input() disabled: boolean;
   
   constructor() { }
